ba6c7e0beb341337eb7ab3eb838135f2a933d740
GhislainINSERM <ghislain.fievet@univ-lorraine.fr>
2022-10-21 09:37:20 +0200

    change icon

--- R/seurselect.r
+++ R/seurselect.r
@@ -144,8 +144,7 @@ server <- function(input, output, session) {
         # plotly::ggplotly(myplot) %>% 
         #   layout(dragmode = "select")
 
-        # plotly::layout(plotly::ggplotly(myplot), dragmode = "select")
-        shiny::layout(plotly::ggplotly(myplot), dragmode = "select")
+        plotly::layout(plotly::ggplotly(myplot), dragmode = "select")
       })
                           
     ## Handle server side large gene set
